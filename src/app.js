9de5ff64e5d5fc429532081b840a7bcc53d014be
Jonathan Silva <contato@jonathansilva.net>
2018-12-23 20:45:24 -0300

    Testes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const restify = require('restify')
-
-const app = restify
+const app = require('restify')
 
 // Não está sendo usado
 var knex = require('knex')({
@@ -25,7 +23,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     next()
-});
+})
 
 app.use('/', indexRoute)
 
