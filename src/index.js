b8f9ffa70248b666ced5d2b233d919eaf4665703
Jonathan Silva <contato@jonathansilva.net>
2018-12-24 21:47:16 -0300

    Testes

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@
 const server = require('./server')
 
 server.listen(8000, '127.0.0.1', function () {
-    console.log('%s listening at %s', server.name, server.url)
+    console.log('%s teste marôto %s', server.name, server.url)
 })
