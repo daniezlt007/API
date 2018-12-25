e8b5d63883816a90e27ab31f21cd8f19199cb32c
Jonathan Silva <contato@jonathansilva.net>
2018-12-24 21:48:47 -0300

    Testes

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ server.get('/', (req, res, next) => {
 })
 
 server.listen(8000, '127.0.0.1', function () {
-    console.log('%s listening at %s', server.name, server.url)
+    console.log('%s teste marôto %s', server.name, server.url)
 })*/
