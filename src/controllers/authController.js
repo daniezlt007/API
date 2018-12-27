29faede2bed53e6ce95af93c64937f6c65c5c9ed
Jonathan Silva <contato@jonathansilva.net>
2018-12-26 22:28:50 -0300

    Testes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const repository = require('../repositories/UserRepository');
+const repository = require('../repositories/authRepository');
 const authService = require('../services/authService');
 const bcrypt = require('bcrypt');
+
+exports.post = async (req, res, next) => {
+    try {
+        const payload = {
+            email: req.body.email,
+            password: bcrypt(req.body.password)
+        };
+        var user = await repository.auth(payload)
+
+        res.status(200).send({
+            token: await authService.generateToken({ ...user[0] }),
+            user: user[0]
+        })
+    } catch (e) {
+        res.status(500).send({
+            message: e.message
+        })
+    }
+}
--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const db = require('../database/config');
+
+exports.auth = async (user) => {
+    const connection = await db.connection();
+    let [rows] = await connection.execute('SELECT uuid, name, profile FROM person WHERE email = ? and password = ?', [user.email, user.password])
+    return rows
+}
