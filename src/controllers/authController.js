c386e276b34b28370d9ca3f603722b18388d2c3e
Jonathan Silva <contato@jonathansilva.net>
2018-12-28 22:29:23 -0300

    Testes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,30 +7,25 @@ const bcrypt = require('bcrypt')
 exports.post = async (req, res, next) => {
     try {
         const saltRounds = 10
-        bcrypt.hash(req.body.password, saltRounds, async (hash) => {
+        bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
             const payload = {
                 phone: req.body.phone,
                 password: hash
             }
 
-            console.log("HASH: ", hash)
-
             var user = await repository.auth(payload)
 
             if (user[0]) {
-                res.send({
+                return res.send({
                     token: await authService.generateToken({ ...user[0] }),
                     user: user[0]
                 })
-            } else {
-                res.send({
-                    message: "Telefone ou senha incorretos"
-                })
             }
+            return res.send({ message: "Telefone ou senha incorretos" })
         })
-    } catch (err) {
+    } catch (e) {
         return res.send({
-            message: err
+            message: e
         })
     }
 }
