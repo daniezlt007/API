46666b1c64ef6e782113ff0f8db67bc6eb854a53
Jonathan Silva <contato@jonathansilva.net>
2018-12-28 22:53:31 -0300

    Testes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,12 +5,12 @@ const authController = require('../controllers/authController')
 const routes = (server) => {
     // Principal
     server.get('/', (req, res, next) => {
-        res.send({hello: 'Minha API'})
+        res.send({ hello: "Minha API" })
         next()
     })
 
     // Autenticação
-    server.post('/login', authController.login)
+    server.get('/login', authController.login)
     server.post('/register', authController.register)
     server.put('/refresh', authController.refresh)
 }
