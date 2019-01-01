e3ee4a68011d6588cf5dd83aa4156070582c8fe8
Jonathan Silva <contato@jonathansilva.net>
2019-01-01 17:54:15 -0300

    Testes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,16 +16,18 @@ exports.login = async (req, res, next) => {
         let data = await repository.login(credentials)
         console.log('DADOS RETORNADOS: ' + data)
 
-        /*if (data[0]) {
+        if (data[0]) {
+            const { id, establishment_id, profile, nickname } = data[0]
+
             return res.send(200, {
-                token: await authService.generateToken({ ...data[0] }),
-                id: data[0].id,
-                establishment_id: data[0].establishment_id,
-                profile: data[0].profile,
-                nickname: data[0].nickname
+                token: await authService.generateToken({ id, establishment_id, profile, nickname }),
+                id: id,
+                establishment_id: establishment_id,
+                profile: profile,
+                nickname: nickname
             })
         }
-        return res.send(400, { message: 'Telefone ou senha incorretos' })*/
+        return res.send(400, { message: 'Telefone ou senha incorretos' })
     } catch (err) {
         return res.send(400, { message: 'Erro: ' + err })
     }
