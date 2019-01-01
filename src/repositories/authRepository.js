51311f6ff25514230ed4974a30f9091d7057c275
Jonathan Silva <contato@jonathansilva.net>
2019-01-01 12:37:42 -0300

    Testes

--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -4,10 +4,13 @@ const db = require('../database/config')
 
 exports.login = async (user) => {
     try {
-        const connection = await db.connection()
+        const connection = db.connection()
         let data = await connection.select('id', 'establishment_id', 'nickname','profile')
         .from('person')
-        .where({'phone': user.phone, 'password': user.password})
+        .where({
+            'phone': user.phone,
+            'password': user.password
+        })
 
         return data
     } catch(error) {
