6c38bc45ab4fc4eb66703c34dd00706d166f32bd
Jonathan Silva <contato@jonathansilva.net>
2019-01-08 22:30:41 -0300

    Testes

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -6,9 +6,10 @@ exports.create = async (user) => {
     const connection = db.connection()
     await connection.insert(user).into('person')
     .then(() => {
+        // Se retirar o return, cai no catch abaixo e retorna Result: undefined
         return connection.select('establishment_id', 'profile').from('person').where('id', user.id)
     }).then((result) => {
-        console.log(result) // Retorna o establishment_id e profile
+        console.log('Result: ' + result) // Retorna o establishment_id e profile
 
         if (result.length === 0) throw new Error('Erro ao recuperar dados')
         return result
