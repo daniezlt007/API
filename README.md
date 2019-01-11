b40eefe9bda6942d3cf92ce84bb04181aa736feb
Jonathan Silva <contato@jonathansilva.net>
2019-01-11 14:53:25 -0300

    Testes

--- README.md
+++ README.md
@@ -9,6 +9,6 @@
 
 1. Primeiro, faça o clone do repositório e no terminal navegue até a pasta
 2. Instale as dependências do projeto com `npm i`
-3. Faça uma cópia do **.env.example** com o nome **.env** e adicione a porta (exemplo: 3000) e informações de conexão com seu banco de dados
+3. Faça uma cópia do **.env.example** com o nome **.env** e adicione a porta (exemplo: 3000), uma chave para o JWT (qualquer string) e informações de conexão com seu banco de dados
 4. Rode o servidor com `npm start`
 5. O *endpoint* do serviço estará disponível em http://127.0.0.1:3000 ou http://localhost:3000
--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -5,13 +5,13 @@ const bcrypt = require('bcrypt')
 
 exports.login = async (user) => {
     const connection = db.connection()
-    const result = await connection.select('id', 'establishment_id', 'nickname', 'profile', 'phone', 'password')
+    const rows = await connection.select('id', 'establishment_id', 'nickname', 'profile', 'phone', 'password')
     .from('person').where('phone', user.phone)
 
-    if (result.length === 0) throw new Error('Telefone não encontrado')
+    if (rows.length === 0) throw new Error('Telefone não encontrado')
 
-    const res = await bcrypt.compare(user.password, result[0].password)
+    const res = await bcrypt.compare(user.password, rows[0].password)
 
     if (!res) throw new Error('Senha inválida')
-    return result
+    return rows
 }
