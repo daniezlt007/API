4b3a815013057fa129ba4662d46ddf6e4ecbaeac
Jonathan Silva <contato@jonathansilva.net>
2019-01-12 19:39:19 -0300

    Testes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,10 +4,23 @@ const uuid = require('uuid/v4')
 const bcrypt = require('bcrypt')
 const repository = require('../repositories/userRepository')
 const authService = require('../services/authService')
-const { validateAll, rule } = require('indicative')
+const joi = require('joi')
 
 exports.register = async (req, res, next) => {
     try {
+        const schema = joi.object().keys({
+            name: joi.string().required(),
+            nickname: joi.string().required(),
+            phone: joi.string().regex(/^\(\d{2}\)\s\d{5}-?\d{4}$/),
+            email: joi.string().email({ minDomainAtoms: 2 }),
+            password: joi.string().min(8).required()
+        })
+
+        const data = await joi.validate(req.body, schema)
+        console.log(data)
+
+        ///////////////////////////////////////////////////////////////////////////////
+
         const id = await uuid()
         const hash = await bcrypt.hash(req.body.password, 10)
 
@@ -20,23 +33,11 @@ exports.register = async (req, res, next) => {
             password: hash
         }
 
-        const rules = {
-            name: 'required|string',
-            nickname: 'required|string',
-            phone: [
-                rule('regex', /\(\d{2}\)\s\d{5}-?\d{4}/)
-            ],
-            email: 'required|email',
-            password: 'required|string'
-        }
-
-        await validateAll(user, rules)
-
-        await repository.create(user)
+        /*await repository.create(user)
 
         return res.send(201, {
             token: await authService.generateToken({ id, profile: 'client' })
-        })
+        })*/
     } catch(error) {
         console.error(error)
         return res.send(400, { error })
