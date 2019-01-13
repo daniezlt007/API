635ba7ca21df9fbbcba9560def655acb8eb784bd
Jonathan Silva <contato@jonathansilva.net>
2019-01-13 15:00:18 -0300

    Organizando querys

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -4,9 +4,18 @@ const db = require('../database/config')
 
 exports.create = async (user) => {
     const connection = db.connection()
-    const rows = await connection.select('phone', 'email').from('person').where('phone', user.phone).orWhere('email', user.email)
+    const rows = await connection('person').select('phone', 'email').where('phone', user.phone).orWhere('email', user.email)
 
     if (rows.length > 0) throw new Error('Telefone ou e-mail, já existem')
 
-    return await connection.insert(user).into('person')
+    return await connection('person').insert(user)
+}
+
+exports.update = async (user) => {
+    const connection = db.connection()
+    const result = await connection('person').select('id').where('id', user.id)
+
+    if (result.length === 0) throw new Error('ID não encontrado')
+
+    return await connection('person').where('id', user.id).update(user)
 }
