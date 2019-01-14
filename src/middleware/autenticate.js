220a52b7e8093b58a9f2c61b0b32bdaa93950d3a
Jonathan Silva <contato@jonathansilva.net>
2019-01-13 22:37:04 -0300

    Ajustes

--- src/middleware/autenticate.js
+++ src/middleware/autenticate.js
@@ -4,17 +4,17 @@ const jwt = require('jsonwebtoken')
 
 const autenticate = (routes) => {
     return async (req, res, next) => {
-      if (!routes.exclusions.includes(req.href())) {
-        const token = req.headers['x-access-token']
+        if (!routes.exclusions.includes(req.href())) {
+            const token = req.headers['x-access-token']
 
-        if (!token) return res.json(401, { message: 'Token não fornecido' })
-        return jwt.verify(token, process.env.SALT_KEY, function (error, decoded) {
-            if (error) return res.json(401, { message: 'Token inválido' })
-            next()
-        })
-      }
-      next()
+            if (!token) return res.json(401, { message: 'Token não fornecido' })
+            return jwt.verify(token, process.env.SALT_KEY, function (error, decoded) {
+                if (error) return res.json(401, { message: 'Token inválido' })
+                next()
+            })
+        }
+        next()
     }
-  }
+}
 
-  module.exports = autenticate
+module.exports = autenticate
