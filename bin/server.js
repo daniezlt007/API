4d81c60ab24ae068afecfbd1b03e1aaece2e679a
Jonathan Silva <contato@jonathansilva.net>
2019-01-13 21:33:26 -0300

    Inclusão do acceptParser e queryParser

--- bin/server.js
+++ bin/server.js
@@ -5,14 +5,19 @@ const restify = require('restify')
 const server = restify.createServer()
 const routes = require('../src/routes/routes')
 
+// Rotas que não precisam de autenticação
+const exclusions = ['/auth']
+
 // CORS
 server.pre(cors.preflight)
 server.use(cors.actual)
 
 // Restify
-/*server.use(restify.plugins.acceptParser(server.acceptable))
-server.use(restify.plugins.queryParser())*/
+server.use(restify.plugins.acceptParser(server.acceptable))
+server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
+
+//server.use(token({ exclusions }))
 
 // Carrega todas as rotas
 routes(server)
