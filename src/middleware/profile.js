d7bc5f93ca64ffb6f8ffb3e4e132c8a64fc0177d
Jonathan Silva <contato@jonathansilva.net>
2019-01-14 22:25:03 -0300

    Remoção de verificação de Token

--- src/middleware/profile.js
+++ src/middleware/profile.js
@@ -5,7 +5,8 @@ const jwt = require('jsonwebtoken')
 exports.isOwner = function (req, res, next) {
     const token = req.headers['x-access-token']
 
-    if (!token) return res.json(401, { message: 'Token inválido' })
+    // Esse if não é relevante aqui pois já é verificado pelo authenticate.js em todas as rotas
+    //if (!token) return res.json(401, { message: 'Token não fornecido' })
     return jwt.verify(token, process.env.JWT_SECRET, function (error, decoded) {
         if (error) return res.json(401, { message: 'Token inválido' })
 
