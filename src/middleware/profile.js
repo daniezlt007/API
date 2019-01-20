c2f55107a6ca5c290f7e8711c369afd15d71bab6
Jonathan Silva <contato@jonathansilva.net>
2019-01-20 18:54:30 -0300

    Finalizando Middleware

--- src/middleware/profile.js
+++ src/middleware/profile.js
@@ -10,19 +10,22 @@ exports.isAuthenticate = (req, res, next) => {
 
     if (!token) return res.json(401, { message: 'Token não fornecido' })
 
-    return jwt.verify(token, JWT_SECRET).then(next).catch(() => res.json(401, { message: 'Token inválido' }))
+    return jwt.verify(token, JWT_SECRET, (error) => {
+        if (error) return res.json(401, { message: 'Token inválido' })
+        next()
+    })
 }
 
 // Rota restrita à Owners
-exports.isOwner = async (req, res, next) => {
+exports.isOwner = (req, res, next) => {
     const token = req.headers['x-access-token']
 
     if (!token) return res.json(401, { message: 'Token não fornecido' })
 
-    const decoded = await jwt.verify(token, JWT_SECRET)
-    //.catch(() => res.json(401, { message: 'Token inválido' }))
+    return jwt.verify(token, JWT_SECRET, (error, decoded) => {
+        if (error) return res.json(401, { message: 'Token inválido' })
 
-    console.log(decoded)
-
-    if (decoded.profile != 'owner') return res.json(403, { message: 'Restrito à Owners' })
+        if (decoded.profile != 'owner') return res.json(403, { message: 'Restrito à Owners' })
+        next()
+    })
 }
