ff3b54740b2a8477a7350328bd8741b87be8f69e
Jonathan Silva <contato@jonathansilva.net>
2019-01-20 18:24:30 -0300

    Refatoração dos Middlewares

--- src/middleware/profile.js
+++ src/middleware/profile.js
@@ -2,28 +2,27 @@
 
 const jwt = require('jsonwebtoken')
 
+const JWT_SECRET = process.env.JWT_SECRET
+
 // Precisa estar autenticado para acessar tal rota
-exports.isAuthenticate = function (req, res, next) {
+exports.isAuthenticate = (req, res, next) => {
     const token = req.headers['x-access-token']
 
     if (!token) return res.json(401, { message: 'Token não fornecido' })
-    return jwt.verify(token, process.env.JWT_SECRET, function (error, decoded) {
-        if (error) return res.json(401, { message: 'Token inválido' })
-        next()
-    })
+
+    return jwt.verify(token, JWT_SECRET).then(next).catch(() => res.json(401, { message: 'Token inválido' }))
 }
 
 // Rota restrita à Owners
-exports.isOwner = function (req, res, next) {
+exports.isOwner = async (req, res, next) => {
     const token = req.headers['x-access-token']
 
     if (!token) return res.json(401, { message: 'Token não fornecido' })
-    return jwt.verify(token, process.env.JWT_SECRET, function (error, decoded) {
-        if (error) return res.json(401, { message: 'Token inválido' })
 
+    const decoded = await jwt.verify(token, JWT_SECRET)
+    //.catch(() => res.json(401, { message: 'Token inválido' }))
 
-        console.log("PROFILE: " + decoded.profile)
-        if (decoded.profile != 'owner') return res.json(403, { message: 'Restrito à Owners' })
-        next()
-    })
+    console.log(decoded)
+
+    if (decoded.profile != 'owner') return res.json(403, { message: 'Restrito à Owners' })
 }
