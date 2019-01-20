ba4328d7be350e1bfac3e2812c6a34f577f6aac5
Jonathan Silva <contato@jonathansilva.net>
2019-01-20 18:25:04 -0300

    Ajustes

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,10 +2,12 @@
 
 const jwt = require('jsonwebtoken')
 
+const JWT_SECRET = process.env.JWT_SECRET
+
 exports.generateToken = async (data) => {
-    return jwt.sign(data, process.env.JWT_SECRET, { expiresIn: '1d' })
+    return jwt.sign(data, JWT_SECRET, { expiresIn: '1d' })
 }
 
 exports.decodeToken = async (token) => {
-    return await jwt.verify(token, process.env.JWT_SECRET)
+    return await jwt.verify(token, JWT_SECRET)
 }
